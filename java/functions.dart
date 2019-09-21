4bbb8108114de3caf7181d7bc75f60a315795b05
probe <probe.park@snowcorp.com>
2019-09-21 19:15:50 +0900

    functional 3

--- java/functions.dart
+++ java/functions.dart
@@ -1,6 +1,6 @@
 main() {
   final values = [1, 2, 3, 4, 5, 10, 50];
-  values.map(scream).forEach(print);
+  values.skip(1).take(3).map(scream).forEach(print);
 }
 
-String scream(int length) => "A${'a' * length}";
+String scream(int length) => "A${'a' * length}h!";
