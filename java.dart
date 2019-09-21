de5f5e8d9d0083aa4ac626ba1b9c8a7ff54aa32b
probe <probe.park@snowcorp.com>
2019-09-21 12:48:41 +0900

    add bicycle class

---
+++ java.dart
@@ -0,0 +1,17 @@
+class Bicycle {
+  int cadence;
+  int speed;
+  int gear;
+
+  Bicycle(this.cadence, this.speed, this.gear);
+
+  @override
+  String toString() {
+    return 'Bicycle{speed: $speed mph}';
+  }
+}
+
+void main() {
+  var bike = Bicycle(2, 0, 1);
+  print(bike);
+}
